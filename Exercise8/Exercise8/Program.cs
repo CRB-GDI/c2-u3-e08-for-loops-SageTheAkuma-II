343344bc720b12cf5cbb054aecc72833ce0e78b3
SageTheAkuma-II <e-sei.walker@s.capregboces.org>
2024-02-29 09:50:22 -0500

    Ready for grading

--- Exercise8/Exercise8/Program.cs
+++ Exercise8/Exercise8/Program.cs
@@ -4,7 +4,25 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Ooga Booga");
+            //Problem 1
+            Console.WriteLine("How many even number?");
+            int maxNumber = int.Parse(Console.ReadLine());
+            int loopCounter;
+            //print even numbers up to requested max
+            for (loopCounter = 0; loopCounter < maxNumber; loopCounter += 2)
+            {
+                Console.WriteLine(loopCounter);
+            }
+            //Problem 2
+            Console.Write("How wide?");
+            int maxWidth = int.Parse(Console.ReadLine());
+            for (int i = 0; i < maxWidth; i++)
+            {
+                //if user enters 5, should print *****
+                Console.Write("*");
+            }
+
         }
     }
 }
+
